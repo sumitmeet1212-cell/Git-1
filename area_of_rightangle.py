a99a032d1879707073b56344314e0613ef07320d
sumitmeet1212-cell <sumitmeet1212@gmail.com>
2025-11-28 18:30:10 +0530

    message

---
+++ area_of_rightangle.py
@@ -0,0 +1,6 @@
+#To find the area of right angle
+#Formula: Area= 0.5*base*height
+base= float(input("Enter the base of triangle in cm: "))
+height= float(input("Enter the height of triangle in cm: "))
+area= 0.5*base*height
+print("The area of the triangle is ", area, "cm^2")
---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+#To find the simple interest
+# Formula: SI=(P*R*T)/100
+principal_amount=float(input("Enter the principal amount"))
+rate=float(input("Enter the interest rate"))
+time=float(input("Enter the time"))
+simple_interest= (principal_amount*rate*time)/100
+print("The simple interest is", simple_interest)
